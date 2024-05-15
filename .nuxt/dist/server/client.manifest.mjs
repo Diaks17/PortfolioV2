5817f786261e6fe1c88512a9ce6ab808db50f44e
Moussadiakite92270 <71708204+Diaks17@users.noreply.github.com>
2024-05-15 10:57:31 +0200

    g

--- .nuxt/dist/server/client.manifest.mjs
+++ .nuxt/dist/server/client.manifest.mjs
@@ -1,1 +1,1 @@
-export { default } from "file:///C:/Users/Diakite/Documents/GitHub/PortfolioV2/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
+export { default } from "file:///Users/diaks/PortfolioV2/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
